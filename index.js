1ebedc31009b8887c20a68e61809afc8352af399
包思远 <116354682+BaoSiYuanCODE@users.noreply.github.com>
2023-01-11 19:38:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,22 +8,18 @@
             $('body').html('');
             briefintroduction();
             break;
-        case '#/updatelog':
+        case '#/approveddocuments':
             $('body').html('');
-            updatelog();
+            approveddocuments();
             break;
-        case '#/download':
+        case '#/licence':
             $('body').html('');
-            download();
-            break;
-        case '#/versionhistory':
-            $('body').html('');
-            versionhistory();
+            licence();
             break;
         default:
-            if (location.hash.search('#/updatelogSub/') != -1) {
+            if (location.hash.search('#/approveddocumentsSub/') != -1) {
                 $('body').html('');
-                updatelogSub();
+                approveddocumentsSub();
             } else {
                 $('body').html('');
                 location.hash = '#/briefintroduction';
@@ -48,30 +44,22 @@ function menu() {
             .attr('class', 'item')
             .attr('id', 'menu2')
             .click(function () {
-                location.hash = '#/updatelog';
+                location.hash = '#/approveddocuments';
             })
-            .text('更新日志')
+            .text('审核通过的文件')
         )
         .append($('<a></a>')
             .attr('class', 'item')
             .attr('id', 'menu3')
             .click(function () {
-                location.hash = '#/download';
+                location.hash = '#/licence';
             })
-            .text('下载')
-        )
-        .append($('<a></a>')
-            .attr('class', 'item')
-            .attr('id', 'menu4')
-            .click(function () {
-                location.hash = '#/versionhistory';
-            })
-            .text('历史版本')
+            .text('PBGPOPL许可证')
         )
 }
 
 function briefintroduction() {
-    document.title = '简介 - PBS系统官网';
+    document.title = '简介 - 思远游戏专利局官网';
     $('body')
         .append(menu())
         .append($('<div></div>')
@@ -82,10 +70,10 @@ function briefintroduction() {
                 .append($('<div></div>')
                     .append($('<p></p>')
                         .append($('<img></img>')
-                            .attr('class', 'ui middle bordered image')
+                            .attr('class', 'ui large bordered image')
                             .css('margin', '5px')
                             .css('margin-left', '10px')
-                            .attr('src', 'https://www.helloimg.com/images/2022/12/29/oCJtMt.jpg')
+                            .attr('src', 'https://www.helloimg.com/images/2022/10/24/Zr6dHK.jpg')
                         )
                     )
                 )
@@ -101,23 +89,20 @@ function briefintroduction() {
                         .append($('<p></p>')
                             .append($('<span></span>')
                                 .attr('class', 'ui big text')
-                                .text('PBS系统官网')
+                                .text('思远游戏专利局官网')
                             )
                         )
                         .append($('<p></p>')
                             .append($('<span></span>')
                                 .attr('class', 'ui large text')
-                                .text('PBS系统是由思远游戏工作室编写的一个系统，长期更新！！')
+                                .text('思远游戏专利局创建目的为维护大家的文件著作权，每一个向思远游戏专利局申请专利的文件审核通过后会获得一个专利号。')
                             )
                         )
                         .append($('<p></p>')
-                            .text('更新日志是PBS系统每一个版本相较于前一个版本的更新记录；')
+                            .text('审核通过的文件记录了所有审核通过的文件的发布地址与专利号等信息；')
                         )
                         .append($('<p></p>')
-                            .text('下载是PBS每一个大版本的最终正式版；')
-                        )
-                        .append($('<p></p>')
-                            .text('历史版本是PBS系统更新历程中每一个大版本的非最终正式版的版本。')
+                            .text('许可证是思远游戏专利局审核通过的文件的著作权保障书。')
                         )
                         .removeAttr('id');
                 })
@@ -126,8 +111,8 @@ function briefintroduction() {
     $('#menu1').attr('class', 'active item');
 }
 
-function updatelog() {
-    document.title = '更新日志 - PBS系统官网';
+function approveddocuments() {
+    document.title = '审核通过的文件 - 思远游戏专利局官网';
     let blogs = [], search = '';
     function refresh() {
         let list = $('#list').empty();
@@ -145,7 +130,7 @@ function updatelog() {
                                 .css('float', 'left')
                                 .text(blogs[i].title)
                                 .click(function () {
-                                    window.open(`/#/updatelogSub/${$(this).attr('click_id')}`)
+                                    window.open(`/#/approveddocumentsSub/${$(this).attr('click_id')}`)
                                 })
                             )
                             .append($('<span></span>')
@@ -155,7 +140,7 @@ function updatelog() {
                             )
                             .append($('<em></em>')
                                 .css('float', 'right')
-                                .text('By 思远游戏工作室')
+                                .text('By 思远游戏专利局')
                             )
                         )
                         .append($('<div></div>')
@@ -176,7 +161,7 @@ function updatelog() {
                 .attr('class', 'ui large relaxed divided list')
                 .attr('id', 'list')
                 .ready(function () {
-                    $.get('https://api.github.com/repos/Philip-Burkart-Game-Studio/PBS/issues?creator=BaoSiYuanCODE&state=open&per_page=10000&page=1', function (json, status) {
+                    $.get('https://api.github.com/repos/baosiyuancode/PBGPOOW/issues?creator=BaoSiYuanCODE&state=open&per_page=10000&page=1', function (json, status) {
                         blogs = json;
                         blogs.sort(function (x, y) {
                             return Date.parse(y.updated_at) - Date.parse(x.updated_at);
@@ -205,9 +190,9 @@ function updatelog() {
     $('#menu2').attr('class', 'active item');
 }
 
-function updatelogSub() {
-    document.title = '更新日志 - PBS系统官网';
-    let id = location.hash.split('#/updatelogSub/')[1];
+function approveddocumentsSub() {
+    document.title = '审核通过的文件 - 思远游戏专利局官网';
+    let id = location.hash.split('#/approveddocumentsSub/')[1];
     $('body')
         .append(menu())
         .append($('<div></div>')
@@ -228,7 +213,7 @@ function updatelogSub() {
                 .attr('id', 'content')
             )
             .ready(function () {
-                $.get(`https://api.github.com/repos/Philip-Burkart-Game-Studio/PBS/issues/${id}`, function (json, status) {
+                $.get(`https://api.github.com/repos/BaoSiYuanCODE/PBGPOOW/issues/${id}`, function (json, status) {
                     marked.setOptions({
                         highlight: function (code) {
                             return hljs.highlightAuto(code).value;
@@ -244,126 +229,54 @@ function updatelogSub() {
     $('#menu2').attr('class', 'active item');
 }
 
-function download() {
-    document.title = '下载 - PBS系统官网';
+function licence() {
+    document.title = 'PBGPOPL许可证 - 思远游戏专利局官网';
+    let json = [];
+    function cout() {
+        $('#licence').empty();
+        marked.setOptions({
+            highlight: function (licence) {
+                return hljs.highlightAuto(licence).value;
+            }
+        });
+        for (let i in json) {
+            let k = json[i];
+            if (k.name=='LICENSE.md') {
+                let thi = $(this);
+                let a;
+                $.get(k.git_url, function (body) {
+                    function b64_to_utf8(str) {
+                        return decodeURIComponent (escape(window.atob(str)));
+                    }                        
+                    body = b64_to_utf8(body.content);
+                    a=body;
+                    console.log(a)
+                    $('#licence')
+                    .append($('<div></div>')
+                        .attr('class', 'content')
+                        .html(marked.parse(a))
+                    );
+                });
+            }
+        }
+        $('pre').attr('style', 'white-space: pre-wrap!important;');
+    }
     $('body')
         .append(menu())
         .append($('<div></div>')
-            .attr('class', 'ui large segment')
-            .css({ 'margin-top': '3%', 'margin-left': '15%', 'margin-right': '15%' })
+            .attr('class', 'ui  big segment')
+            .css({ 'margin-top': '5%', 'margin-left': '14%', 'margin-right': '14%' })
             .append($('<div></div>')
-                .attr('class', 'ui two column very relaxed grid')
-                .append($('<div></div>')
-                    .append($('<p></p>')
-                        .append($('<img></img>')
-                            .attr('class', 'ui small bordered image')
-                            .css('margin', '5px')
-                            .css('margin-left', '10px')
-                            .attr('src', 'https://www.helloimg.com/images/2022/12/29/oCJtMt.jpg')
-                        )
-                    )
-                )
-                .append($('<div></div>')
-                    .attr('class', 'column')
-                    .attr('id', 'my')
-                    .append($('<p></p>')
-                        .text('Loading...')
-                    )
-                )
+                .attr('class', 'ui cards')
+                .attr('id', 'licence')
                 .ready(function () {
-                    $('#my').empty()
-                        .append($('<p></p>')
-                            .append($('<span></span>')
-                                .attr('class', 'ui large text')
-                                .text('PBS 1.0系统 [PBS 1.0(2022-12-31)系统]')
-                            )
-                        )
-                        .append($('<a></a>')
-                            .attr('href', 'https://www.luogu.com.cn/training/265688')
-                            .text('PBS 1.0系统下载链接')
-                        )
-                        .removeAttr('id');
+                    $.get('https://api.github.com/repos/baosiyuancode/PBGPOOW/contents/', function (body, status) {
+                        json = body;
+                        cout();
+                    })
                 })
             )
-            .append($('<div></div>')
-                .attr('class', 'ui two column very relaxed grid')
-                .append($('<div></div>')
-                    .append($('<p></p>')
-                        .append($('<img></img>')
-                            .attr('class', 'ui small bordered image')
-                            .css('margin', '5px')
-                            .css('margin-left', '10px')
-                            .attr('src', 'https://www.helloimg.com/images/2022/12/29/oCJtMt.jpg')
-                        )
-                    )
-                )
-                .append($('<div></div>')
-                    .attr('class', 'column')
-                    .attr('id', 'my')
-                    .append($('<p></p>')
-                        .text('Loading...')
-                    )
-                )
-                .ready(function () {
-                    $('#my').empty()
-                        .append($('<p></p>')
-                            .append($('<span></span>')
-                                .attr('class', 'ui large text')
-                                .text('PBS 2.0系统 []')
-                            )
-                        )
-                        .append($('<a></a>')
-                            .attr('href', '')
-                            .text('PBS 2.0系统下载链接')
-                        )
-                        .removeAttr('id');
-                })
-            )
-        );
+        )
+    cout();    
     $('#menu3').attr('class', 'active item');
-}
-
-function versionhistory() {
-    document.title = '历史版本 - PBS系统官网';
-    $('body')
-        .append(menu())
-        .append($('<div></div>')
-            .attr('class', 'ui large segment')
-            .css({ 'margin-top': '3%', 'margin-left': '15%', 'margin-right': '15%' })
-            .append($('<div></div>')
-                .attr('class', 'ui two column very relaxed grid')
-                .append($('<div></div>')
-                    .append($('<p></p>')
-                        .append($('<img></img>')
-                            .attr('class', 'ui small bordered image')
-                            .css('margin', '5px')
-                            .css('margin-left', '10px')
-                            .attr('src', 'https://www.helloimg.com/images/2022/12/29/oCJtMt.jpg')
-                        )
-                    )
-                )
-                .append($('<div></div>')
-                    .attr('class', 'column')
-                    .attr('id', 'my')
-                    .append($('<p></p>')
-                        .text('Loading...')
-                    )
-                )
-                .ready(function () {
-                    $('#my').empty()
-                        .append($('<p></p>')
-                            .append($('<span></span>')
-                                .attr('class', 'ui large text')
-                                .text('PBS 2.0(2023-1-1)系统')
-                            )
-                        )
-                        .append($('<a></a>')
-                            .attr('href', 'https://www.luogu.com.cn/training/265981')
-                            .text('PBS 2.0(2023-1-1)系统下载链接')
-                        )
-                        .removeAttr('id');
-                })
-            )
-        );
-    $('#menu4').attr('class', 'active item');
 }
